78422f1465874c931e2bfa195d0363abad5078f5
Daniel Schauenberg <d@unwiredcouch.com>
2012-09-15 18:35:44 -0400

    add elinks cookbook

---
+++ cookbooks/elinks/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: elinks
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "elinks" do
+  action :install
+end
