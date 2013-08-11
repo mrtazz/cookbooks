4cdf09d1d56d00f0493773c9178a44465029cfc3
Daniel Schauenberg <d@unwiredcouch.com>
2013-08-11 14:23:11 +0200

    add sshd config to security cookbook

--- cookbooks/security/recipes/freebsd.rb
+++ cookbooks/security/recipes/freebsd.rb
@@ -1,3 +1,16 @@
 package "portaudit" do
   action :install
 end
+
+template "/etc/ssh/sshd_config" do
+  source "sshd_config.erb"
+  owner "root"
+  group "wheel"
+  mode 0644
+  notifies :restart, "service[sshd]"
+end
+
+service "sshd" do
+  supports [:start, :stop, :restart]
+  action :enable
+end
