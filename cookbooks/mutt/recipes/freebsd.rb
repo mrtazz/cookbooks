388dad5ef97775ecd672c8ecd70d171e5b5ed126
Daniel Schauenberg <d@unwiredcouch.com>
2013-08-11 14:24:08 +0200

    update compile flags for mutt on freebsd

--- cookbooks/mutt/recipes/freebsd.rb
+++ cookbooks/mutt/recipes/freebsd.rb
@@ -2,8 +2,8 @@ script "install mutt" do
   interpreter "sh"
   cwd "/usr/ports/mail/mutt"
   code <<-EOS
-  make -DBATCH -DWITH_MUTT_IMAP_HEADER_CACHE -DWITH_MUTT_MAILDIR_HEADER_CACHE \
-  -DWITH_MUTT_SMTP -DWITH_TOKYOCABINET install clean
+  make -DBATCH -DWITH_IMAP_HEADER_CACHE -DWITH_MAILDIR_HEADER_CACHE \
+  -DWITH_MUTT_SMTP -DWITH_TOKYOCABINET -DWITH_SIDEBAR_PATCH install clean
   EOS
   not_if { File.exists? "/usr/local/bin/mutt" }
 end
