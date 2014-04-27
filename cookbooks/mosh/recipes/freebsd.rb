785b55265fb5863b971861030e77c8977fc617ec
Daniel Schauenberg <d@unwiredcouch.com>
2014-04-27 15:50:09 -0400

    install mosh from pkgng

--- cookbooks/mosh/recipes/freebsd.rb
+++ cookbooks/mosh/recipes/freebsd.rb
@@ -1,8 +1,1 @@
-script "install mosh" do
-  interpreter "sh"
-  cwd "/usr/ports/net/mosh"
-  code <<-EOS
-  make -DBATCH install clean
-  EOS
-  not_if { File.exists? "/usr/local/bin/mosh-server" }
-end
+package "net/mosh"
