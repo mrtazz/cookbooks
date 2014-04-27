1d3fe46e750a28b447f35d8a9aced9f4759ba50d
Daniel Schauenberg <d@unwiredcouch.com>
2014-04-27 15:48:20 -0400

    cron portsnap and freebsd-update

--- cookbooks/cron/recipes/freebsd.rb
+++ cookbooks/cron/recipes/freebsd.rb
@@ -2,5 +2,12 @@ cron "portsnap update" do
   user "root"
   minute "0"
   hour "3"
-  command "portsnap -I cron update && pkg_version -vIL="
+  command "/usr/sbin/portsnap -I cron update"
+end
+
+cron "freebsd update" do
+  user "root"
+  minute "33"
+  hour "2"
+  command "/usr/sbin/freebsd-update cron"
 end
