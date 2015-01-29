a478287d30cbc274ee155e18a7193f224a308d86
Daniel Schauenberg <d@unwiredcouch.com>
2015-01-28 19:47:48 -0500

    make check_freebsd_update.sh way less awful

--- cookbooks/nagios/files/default/checks/check_freebsd_update.sh
+++ cookbooks/nagios/files/default/checks/check_freebsd_update.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-RELREGEX='[0-9.]*\?-RELEASE-p[0-9]*\?'
-NEEDSUPDATING=$(sed 's/\[ ! -t 0 \]/false/' /usr/sbin/freebsd-update | sh -s fetch | grep "files will be ")
+TAGFILE="/var/db/freebsd-update/tag"
 
-if [ "z${NEEDSUPDATING}" == "z" ]; then
-  echo "FreeBSD installation up-to-date."
+if [ ! -f ${TAGFILE} ]; then
+  echo "Couldn't find ${TAGFILE} to check for version."
+  exit 3
+fi
+
+CURRENT_VERSION=$(uname -r)
+UPDATE_BASE=$(cut -f 3 -d '|' ${TAGFILE})
+UPDATE_PATCH=$(cut -f 4 -d '|' ${TAGFILE})
+UPDATE_VERSION="${UPDATE_BASE}-p${UPDATE_PATCH}"
+
+if [ "z${CURRENT_VERSION}" == "z${UPDATE_VERSION}" ]; then
+  echo "FreeBSD installation up-to-date on ${CURRENT_VERSION}."
   exit 0
 else
-  ISREL=$(uname -r)
-  SHOULDREL=$(echo "${NEEDSUPDATING}" | grep -o -m1 "${RELREGEX}")
-  echo "FreeBSD installation is on ${ISREL} and should be on ${SHOULDREL}"
+  echo "FreeBSD installation is on ${CURRENT_VERSION} and should be on ${UPDATE_VERSION}."
   exit 2
 fi
