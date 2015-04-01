a51b4d5bd01f559d66ce4911b138c1559ca3f697
Daniel Schauenberg <d@unwiredcouch.com>
2015-04-01 08:42:28 -0400

    refactor mrtazz procmail setup

--- cookbooks/homedirs/recipes/mrtazz.rb
+++ cookbooks/homedirs/recipes/mrtazz.rb
@@ -87,12 +87,39 @@ mrtazz_data_bag = data_bag_item('users', 'mrtazz')
 spams = mrtazz_data_bag["spamaddresses"]
 maillists = mrtazz_data_bag["maillists"]
 
-template "#{homedir}/.procmailrc" do
-  source "mrtazz/procmailrc.erb"
-  owner "mrtazz"
+procmail_files = [
+  { :source => "mrtazz/procmailrc.erb",
+    :target => "#{homedir}/.procmailrc" },
+  { :source => "mrtazz/procmail-spam.rc.erb",
+    :target => "#{homedir}/.procmail/spam.rc" },
+  { :source => "mrtazz/procmail-maillists.rc.erb",
+    :target => "#{homedir}/.procmail/maillists.rc" },
+]
+
+directory "#{homedir}/.procmail" do
+  owner 'mrtazz'
   group mrtazz_group
-  mode "0600"
-  variables({ :spams => spams, :maillists => maillists })
+  mode '0700'
+  action :create
+end
+
+procmail_files.each do |procfile|
+
+  template procfile[:target] do
+    source procfile[:source]
+    owner "mrtazz"
+    group mrtazz_group
+    mode "0600"
+    variables({ :spams => spams, :maillists => maillists })
+  end
+
+end
+
+file "#{homedir}/.procmail/custom.rc" do
+  owner 'mrtazz'
+  group mrtazz_group
+  mode '0600'
+  action :create_if_missing
 end
 
 template "#{homedir}/.forward" do
