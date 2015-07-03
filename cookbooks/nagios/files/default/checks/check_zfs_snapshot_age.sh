8f6a585814389df0d41bac1925144f31fb9a3803
Daniel Schauenberg <d@unwiredcouch.com>
2015-07-03 19:10:25 -0400

    adapt backup setup to support Archive

--- cookbooks/nagios/files/default/checks/check_zfs_snapshot_age.sh
+++ cookbooks/nagios/files/default/checks/check_zfs_snapshot_age.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
-# nagios script to check age of backup snapshots
+# nagios script to check age of snapshots
 
 YESTERDAY=`date -v-1d +%Y-%m-%d`
 EXITCODE=0
+VOLUME="backup"
 
-for backup in $(zfs list -r -o name -H backup | grep "/"); do
-  zfs list -r -t snapshot ${backup}@${YESTERDAY}-23:00:00 > /dev/null 2>&1
+if [ -n "${1}" ]; then
+  VOLUME=${1}
+fi
+
+for vol in $(ls /${VOLUME} | grep -v Archive); do
+  zfs list -r -t snapshot ${VOLUME}/${vol}@${YESTERDAY}-23:00:00 > /dev/null 2>&1
   if [ $? != 0 ]; then
-    zfs list -r -t snapshot ${backup}@${YESTERDAY}-23:00:01 > /dev/null 2>&1
+    zfs list -r -t snapshot ${VOLUME}/${vol}@${YESTERDAY}-23:00:01 > /dev/null 2>&1
     if [ $? != 0 ]; then
-      echo "Snapshot of ${backup} missing for ${YESTERDAY}."
+      echo "Snapshot of ${vol} missing for ${YESTERDAY}."
       EXITCODE=2
     fi
   fi
 done
 
 if [ ${EXITCODE} == 0 ]; then
-  echo "All backup volumes were snapshotted on ${YESTERDAY}."
+  echo "All volumes on '${VOLUME}' were snapshotted on ${YESTERDAY}."
 fi
 
 exit ${EXITCODE}
