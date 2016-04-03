d933ac5cca7b64e633c9af5d6348b6f537d7fabc
Daniel Schauenberg <d@unwiredcouch.com>
2016-04-03 14:36:05 -0400

    upgrade php to 5.6

--- cookbooks/php/recipes/default.rb
+++ cookbooks/php/recipes/default.rb
@@ -7,14 +7,26 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package "php5" do
+php_version = "56"
+
+package "php#{php_version}" do
   action :install
 end
 
-package "mod_php5" do
+package "mod_php#{php_version}" do
   action :install
 end
 
-package "php5-openssl" do
+package "php#{php_version}-openssl" do
+  action :install
+end
+
+# let's also install the sqlite drivers
+
+package "php56-sqlite3" do
+  action :install
+end
+
+package "php56-pdo_sqlite" do
   action :install
 end
