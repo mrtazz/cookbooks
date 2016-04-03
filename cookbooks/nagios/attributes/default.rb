f17f79930a47e79b178ebf6914380680049f37cc
Daniel Schauenberg <d@unwiredcouch.com>
2016-04-03 14:38:21 -0400

    remove unneeded attributes from nagios cookbook

--- cookbooks/nagios/attributes/default.rb
+++ cookbooks/nagios/attributes/default.rb
@@ -1,13 +1,1 @@
 default[:nagios] = {}
-
-default[:nagios][:external_fake_hosts] = {
-  :services_file => "external_fake_services",
-  :hosts => [
-    {:name => "ExternalChecks", :alias => "External Checks"}
-  ]
-}
-
-default[:nagios][:home_fake_hosts] = {
-  :services_file => "home_fake_services",
-  :hosts => []
-}
